64f392926f47b47d629746c40089a9cc4e36edfc
woomozzi <95073031+woomozzi@users.noreply.github.com>
2023-07-31 20:18:05 +0900

    [Silver III] Title: 바이러스, Time: 68 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -0,0 +1,21 @@
+
+n = int(input())
+m = int(input())
+
+emplist = [[] for _ in range(n+1)]
+
+for i in range(m):
+    x, y = map(int, input().split())
+    emplist[x].append(y)
+    emplist[y].append(x)
+
+# virus = [0 for _ in range(n)]
+virus = [0]*(n+1)
+
+def virusdfs(demplist, v, dvirus):
+    virus[v] = 1
+    for i in demplist[v] :
+        if virus[i] == 0:
+            virusdfs(demplist, i, dvirus)
+virusdfs(emplist, 1, virus)
+print(virus.count(1)-1)
