ec372efc07ded800c01d969d22d468ce4e57df9b
woomozzi <95073031+woomozzi@users.noreply.github.com>
2023-08-05 14:47:53 +0900

    [Bronze III] Title: 직각삼각형, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -0,0 +1,9 @@
+while True:
+    triangle = list(map(int, input().split()))
+    triangle.sort()
+    if sum(triangle) == 0:
+        break
+    if (triangle[0]**2) + (triangle[1]**2) == triangle[2]**2 :
+        print('right')
+    else:
+        print('wrong')
